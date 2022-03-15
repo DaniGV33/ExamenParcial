828dc0aadca3d202921b842198f43474253d9d45
Daniel García <100222258+DaniGV33@users.noreply.github.com>
2022-03-15 09:41:34 -0600

    Adición de programa python
    Se añadió un programa de ingreso y suma de carácteres

---
+++ python.py
@@ -0,0 +1,12 @@
+print()
+print('Qué ondaaaaa?')
+n1=float(input('Ingrese un número para sumar: '))
+n2=float(input('Ingrese un segundo número para sumar: '))
+suma=n1+n2
+print(n1,'+',n2,'= si')
+print()
+print('No, es broma, la respuesta es',suma)
+print()
+
+print('Coded by:')
+print('Daniel Augusto García Villatoro - 1532522')
